0d0d0e914d3db8e795dfb07cc89f256ed5600812
Vivian Li <vili4418@colorado.edu>
2024-08-31 11:29:31 -0700

    lab 00 + lecture notes

---
+++ main.h
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <cstdlib>
+#include <sstream>
+#include <string>
+#include <vector>
+#include "math.h"
+using std :: string;
+using std :: vector;
+using std :: stringstream;
+using std :: cout;
+using std :: endl;
+
+
