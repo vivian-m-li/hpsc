63fe66d9041c949ba0021254375e7e5167e900d0
Vivian Li <vili4418@colorado.edu>
2024-09-13 14:45:22 -0600

    lab2

--- lab2/src/mpiInfo.h
+++ lab2/src/mpiInfo.h
@@ -1,10 +1,8 @@
-void Exit()
-{
+void Exit() {
   MPI_Barrier(MPI_COMM_WORLD);
   MPI_Finalize();
   exit(0);
 }
-
 
 //  ==
 //  ||
@@ -12,29 +10,27 @@ void Exit()
 //  ||
 //  ==
 
-class mpiInfo
-{
+class mpiInfo {
  public:
-
   int myPE;
   int numPE;
-  int nRealx,  nRealy;
+  int nRealx, nRealy;
   int nPEx, nPEy;
-  int iPE , jPE;
-  int iMin, iMax, jMin, jMax ; // The global i-j numbers on this processor
+  int iPE, jPE;
+  int iMin, iMax, jMin, jMax;  // The global i-j numbers on this processor
   int nei_n, nei_s, nei_e, nei_w;
   int countx, county;
 
   double *phiL, *phiR;
   double *phiT, *phiB;
-    
-  double *phiSend_n,  *phiSend_s;
-  double *phiSend_e,  *phiSend_w;
-  double *phiRecv_n,  *phiRecv_s, *phiRecv_e,  *phiRecv_w;
-  
-  MPI_Status  status;
-  int         err;
-  int         tag;
+
+  double *phiSend_n, *phiSend_s;
+  double *phiSend_e, *phiSend_w;
+  double *phiRecv_n, *phiRecv_s, *phiRecv_e, *phiRecv_w;
+
+  MPI_Status status;
+  int err;
+  int tag;
   MPI_Request request;
 
   //  -
@@ -66,105 +62,155 @@ class mpiInfo
   //  |
   //  |
   //  -
-  
 
-  void GridDecomposition(int _nPEx, int _nPEy, int nCellx , int nCelly)
-  {
-
+  void GridDecomposition(int _nPEx, int _nPEy, int nCellx, int nCelly) {
     nRealx = nCellx;
     nRealy = nCelly;
 
     // Store and check incoming processor counts
-    
+
     nPEx = _nPEx;
     nPEy = _nPEy;
-    
-    if (nPEx*nPEy != numPE)
-      {
-    	if ( myPE == 0 ) cout << "Fatal Error:  Number of PEs in x-y directions do not add up to numPE" << endl;
-    	MPI_Barrier(MPI_COMM_WORLD);
-    	MPI_Finalize();
-    	exit(0);
-      }
-    
-    // Get the i-j location of this processor, given its number.  See figure above:
-    
-    jPE = int(myPE/nPEx);
-    iPE = myPE - jPE*nPEx;
+
+    if (nPEx * nPEy != numPE) {
+      if (myPE == 0)
+        cout << "Fatal Error:  Number of PEs in x-y directions do not add up "
+                "to numPE"
+             << endl;
+      MPI_Barrier(MPI_COMM_WORLD);
+      MPI_Finalize();
+      exit(0);
+    }
+
+    // Get the i-j location of this processor, given its number.  See figure
+    // above:
+
+    jPE = int(myPE / nPEx);
+    iPE = myPE - jPE * nPEx;
 
     // Set neighbor values
 
     nei_n = nei_s = nei_e = nei_w = -1;
 
-    if ( iPE > 0      )
-      {
-	nei_w = myPE - 1    ;
-      }
+    if (iPE > 0) {
+      nei_w = myPE - 1;  // west neighbor
+    }
 
-    // TO-DO in Lab : nei_s
-    // TO-DO in Lab : nei_e
-    // TO-DO in Lab : nei_w
+    if (iPE < nPEx - 1) {
+      nei_e = myPE + 1;  // east neighbor
+    }
+
+    if (jPE > 0) {
+      nei_s = myPE - nPEy;  // south neighbor, subtract a row
+    }
+
+    if (jPE < nPEy - 1) {
+      nei_n = myPE + nPEy;  // north neighbor, add a row
+    }
 
     countx = nRealx + 2;
     county = nRealy + 2;
-    
-    phiL = new double [ county ];	
-    phiR = new double [ county ];	
-    phiT = new double [ countx ];	
-    phiB = new double [ countx ];
-    
-    phiSend_n = new double [ countx ];
-    phiSend_s = new double [ countx ];
-    phiSend_e = new double [ county ];
-    phiSend_w = new double [ county ];
-    phiRecv_n = new double [ countx ];
-    phiRecv_s = new double [ countx ];
-    phiRecv_e = new double [ county ];
-    phiRecv_w = new double [ county ];
+
+    phiL = new double[county];
+    phiR = new double[county];
+    phiT = new double[countx];
+    phiB = new double[countx];
+
+    phiSend_n = new double[countx];
+    phiSend_s = new double[countx];
+    phiSend_e = new double[county];
+    phiSend_w = new double[county];
+    phiRecv_n = new double[countx];
+    phiRecv_s = new double[countx];
+    phiRecv_e = new double[county];
+    phiRecv_w = new double[county];
 
     tag = 0;
   }
 
-  void ExchangeBoundaryInfo(VD &Solution, VD &b)
-  {
-	sLOOP phiSend_n[s] = 0.;
-	sLOOP phiSend_s[s] = 0.;
-	tLOOP phiSend_e[t] = 0.;
-	tLOOP phiSend_w[t] = 0.;
-	
-	// ----------------------------------------------
-	// Parallel communication on PE Boundaries       ** See fd.h for tLOOP and sLOOP macros **
-	// ----------------------------------------------
+  void ExchangeBoundaryInfo(VD &Solution, VD &b) {
+    sLOOP phiSend_n[s] = 0.;
+    sLOOP phiSend_s[s] = 0.;
+    tLOOP phiSend_e[t] = 0.;
+    tLOOP phiSend_w[t] = 0.;
 
-	// (1.1) Put values into communication arrays
-	
-	sLOOP phiSend_n[s] = Solution[ /* TO-DO in Lab */) ];
-	sLOOP phiSend_s[s] = Solution[ /* TO-DO in Lab */) ];
-	tLOOP phiSend_w[t] = Solution[ /* TO-DO in Lab */) ];
-	tLOOP phiSend_e[t] = Solution[ /* TO-DO in Lab */) ];
+    // ----------------------------------------------
+    // Parallel communication on PE Boundaries       ** See fd.h for tLOOP and
+    // sLOOP macros **
+    // ----------------------------------------------
 
-	// (1.2) Send them to neighboring PEs
+    // (1.1) Put values into communication arrays
 
-        if ( nei_n >= 0 )  err = MPI_Isend( /* TO-DO in Lab */  , &request );
-	if ( nei_s >= 0 )  err = MPI_Isend( /* TO-DO in Lab */  , &request );
-	if ( nei_e >= 0 )  err = MPI_Isend( /* TO-DO in Lab */  , &request );
-	if ( nei_w >= 0 )  err = MPI_Isend( /* TO-DO in Lab */  , &request );
+    sLOOP phiSend_n[s] = Solution[s];
+    sLOOP phiSend_s[s] = Solution[s];
+    tLOOP phiSend_w[t] = Solution[t];
+    tLOOP phiSend_e[t] = Solution[t];
 
-	// (1.3) Receive values from neighobring PEs' physical boundaries.
-	
-        if ( nei_n >= 0 ) { err = MPI_Irecv( /* TO-DO in Lab */ , &request );   MPI_Wait(&request,&status); } 
-	if ( nei_s >= 0 ) { err = MPI_Irecv( /* TO-DO in Lab */ , &request );   MPI_Wait(&request,&status); }
-	if ( nei_e >= 0 ) { err = MPI_Irecv( /* TO-DO in Lab */ , &request );   MPI_Wait(&request,&status); }
-	if ( nei_w >= 0 ) { err = MPI_Irecv( /* TO-DO in Lab */ , &request );   MPI_Wait(&request,&status); }
-	
-	// (1.4) Update BCs using the exchanged information
-	
-        if ( nei_n >= 0 ) sLOOP b[ /* TO-DO in Lab */ ] = phiRecv_n[s] ; 
-	if ( nei_s >= 0 ) sLOOP b[ /* TO-DO in Lab */ ] = phiRecv_s[s] ; 
-	if ( nei_e >= 0 ) tLOOP b[ /* TO-DO in Lab */ ] = phiRecv_e[t] ; 
-	if ( nei_w >= 0 ) tLOOP b[ /* TO-DO in Lab */ ] = phiRecv_w[t] ; 
+    // (1.2) Send them to neighboring PEs
+
+    // int MPI_Isend(
+    //       const void* buffer,
+    //       int count,
+    //       MPI_Datatype datatype,
+    //       int recipient,
+    //       int tag,
+    //       MPI_Comm communicator,
+    //       MPI_Request* request);
+
+    if (nei_n >= 0)
+      err = MPI_Isend(
+          &phiSend_n, nRealx + 2, MPI_DOUBLE, nei_n, 0, MPI_COMM_WORLD,
+          &request);  // send the row of phi values (phiSend_n) of length nRealx
+                      // + 2 (real + ghost cells) to the north
+    if (nei_s >= 0)
+      err = MPI_Isend(&phiSend_s, nRealx + 2, MPI_DOUBLE, nei_s, 0,
+                      MPI_COMM_WORLD, &request);
+    if (nei_e >= 0)
+      err = MPI_Isend(&phiSend_e, nRealy + 2, MPI_DOUBLE, nei_e, 0,
+                      MPI_COMM_WORLD, &request);
+    if (nei_w >= 0)
+      err = MPI_Isend(&phiSend_w, nRealy + 2, MPI_DOUBLE, nei_w, 0,
+                      MPI_COMM_WORLD, &request);
+
+    // (1.3) Receive values from neighboring PEs' physical boundaries.
+
+    // int MPI_Irecv(void* buffer,
+    //           int count,
+    //           MPI_Datatype datatype,
+    //           int sender,
+    //           int tag,
+    //           MPI_Comm communicator,
+    //           MPI_Request* request);
+
+    if (nei_n >= 0) {
+      err = MPI_Irecv(&phiRecv_n, nRealx + 2, MPI_DOUBLE, nei_n, 0,
+                      MPI_COMM_WORLD, &request);
+      MPI_Wait(&request, &status);
+    }
+    if (nei_s >= 0) {
+      err = MPI_Irecv(&phiRecv_s, nRealx + 2, MPI_DOUBLE, nei_s, 0,
+                      MPI_COMM_WORLD, &request);
+      MPI_Wait(&request, &status);
+    }
+    if (nei_e >= 0) {
+      err = MPI_Irecv(&phiRecv_e, nRealy + 2, MPI_DOUBLE, nei_e, 0,
+                      MPI_COMM_WORLD, &request);
+      MPI_Wait(&request, &status);
+    }
+    if (nei_w >= 0) {
+      err = MPI_Irecv(&phiRecv_w, nRealy + 2, MPI_DOUBLE, nei_w, 0,
+                      MPI_COMM_WORLD, &request);
+      MPI_Wait(&request, &status);
+    }
+
+    // (1.4) Update BCs using the exchanged information
+
+    if (nei_n >= 0)
+      sLOOP b[pid(s, nRealy)] = phiRecv_n[s];  // update the entire top row
+    if (nei_s >= 0) sLOOP b[pid(s, 1)] = phiRecv_s[s];
+    if (nei_e >= 0) tLOOP b[pid(nRealx, t)] = phiRecv_e[t];
+    if (nei_w >= 0) tLOOP b[pid(1, t)] = phiRecv_w[t];
   }
-  
-  int pid(int i,int j) { return (i+1) + (j)*(nRealx+2); }  
 
+  int pid(int i, int j) { return (i + 1) + (j) * (nRealx + 2); }
 };
